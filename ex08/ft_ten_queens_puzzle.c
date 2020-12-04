d52adf41bd847a045a5fce50d6dd66ee8bbbf1dd
Ubuntu <ubuntu@ip-172-31-3-183.eu-central-1.compute.internal>
2020-12-04 10:56:47 +0000

    WIP

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -12,63 +12,57 @@
 
 #include <unistd.h>
 
-char check_valid(char *rows, int n)
+void ft_putchar(char c)
 {
-	char	board[10][10];
-	int		i;
-	int		j;
-	
-	i = 0;
-	while (i < 10)
-	{
-		j = 0;
-		while (j < 10)
-		{
-			board[i][j] = 0;
-			j++;
-		}
-		i++;
-	}
-	while (x < n)
-	{
-		board[i][y] = 1;
-		board[x][i] = 1;
-		if ((y - x + i >= 0) && (y - x + i) < 10)
-			board[i][y - x + i] = 1;
-		if ((y + x - i >= 0) && (y - x + i) < 10)
-			board[i][y + x - i] = 1;
-		i++;
-	}
-
-	return (1);
+    write(1, &c, 1);
 }
 
-int		ft_iter(char *rows, int n)
+char is_valid(char *rows, int n)
+{
+    int i;
+
+    i = 0;
+    while (i < n)
+    {
+        if (rows[i] == rows[n]
+            || rows[i] + n - i == rows[n]
+            || rows[i] - n + i == rows[n])
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+int	    next_queen(char *rows, int n)
 {
 	int		i;
 	int		sum;
 
-	if (n >= 10)
+        i = 0;
+        if (n >= 10)
 	{
 		// write result and return 1
 		while (i < 10)
-			write(1, '0' + rows[i++], 1);
-		write(1, '\n', 1);
+			ft_putchar('0' + rows[i++]);
+		ft_putchar('\n');;
 		return (1);
 	}
-	sum = 0;
+        if (!is_valid(rows, n))
+            return (0);
+        sum = 0;
 	while (i < 10)
 	{
 		rows[n] = i;
-		sum += ft_iter(rows, n + 1);
+		sum += next_queen(rows, n + 1);
 		i++;
 	}
 	return (sum);
 }
-int		ft_ten_queen_puzzle(void)
+
+int	    ft_ten_queen_puzzle(void)
 {
 	char	rows[10];
-	return (ft_iter(rows, 0));
+	return (next_queen(rows, 0));
 }
 
 int main()
