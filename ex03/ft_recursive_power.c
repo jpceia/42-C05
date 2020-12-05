602b0f66154aeb354295d6c53194bef54fc86fe5
Joao pedro Ceia <jceia@c2r1s7.42lisboa.com>
2020-12-05 02:09:56 +0000

    simplified formula for ft_recursive_power

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 20:09:00 by jceia             #+#    #+#             */
-/*   Updated: 2020/12/03 19:13:21 by jceia            ###   ########.fr       */
+/*   Updated: 2020/12/05 02:09:11 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,5 @@ int	ft_recursive_power(int nb, int power)
 		return (0);
 	if (power == 0)
 		return (1);
-	if (power == 1)
-		return (nb);
 	return (nb * ft_recursive_power(nb, power - 1));
 }
