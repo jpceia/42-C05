27c7edd7c6a1af8d9b6585a3b8524dcc2749e8e1
Joao pedro Ceia <jceia@c2r1s7.42lisboa.com>
2020-12-05 02:26:11 +0000

    WIP ten queens puzzle

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -6,66 +6,66 @@
 /*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 21:15:40 by jceia             #+#    #+#             */
-/*   Updated: 2020/12/03 22:29:39 by jceia            ###   ########.fr       */
+/*   Updated: 2020/12/05 02:24:19 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-char is_valid(char *rows, int n)
+int		is_valid(int *rows, int n)
 {
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (rows[i] == rows[n]
-            || rows[i] + n - i == rows[n]
-            || rows[i] - n + i == rows[n])
-            return (0);
-        i++;
-    }
-    return (1);
+	int i;
+	i = 0;
+	while (i < n)
+	{
+		if ((rows[i] == rows[n])
+			|| (rows[i] + n - i == rows[n])
+			|| (rows[i] - n + i == rows[n]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
-
-int	    next_queen(char *rows, int n)
+ 
+int		add_queen(int *rows, int n)
 {
 	int		i;
 	int		sum;
 
-        i = 0;
-        if (n >= 10)
+	if (!is_valid(rows, n - 1))
+		return (0);
+	if (n >= 10)
 	{
-		// write result and return 1
+		i = 0;
 		while (i < 10)
 			ft_putchar('0' + rows[i++]);
-		ft_putchar('\n');;
+		ft_putchar('\n');
 		return (1);
 	}
-        if (!is_valid(rows, n))
-            return (0);
-        sum = 0;
+	sum = 0;
+	i = 0;
 	while (i < 10)
 	{
 		rows[n] = i;
-		sum += next_queen(rows, n + 1);
+		sum += add_queen(rows, n + 1);
 		i++;
 	}
 	return (sum);
 }
 
-int	    ft_ten_queen_puzzle(void)
+int		ft_ten_queen_puzzle(void)
 {
-	char	rows[10];
-	return (next_queen(rows, 0));
+	int	rows[10];
+	return (add_queen(rows, 0));
 }
 
-int main()
+int		main(void)
 {
-	ft_ten_queen_puzzle();
+	int n = ft_ten_queen_puzzle();
+	printf("Possible combinations: %d\n", n);
 }
